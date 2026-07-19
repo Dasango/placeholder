00e5e8416cb99e187e5841531ce097b3dcd0f529
dasango <dasango@uce.edu.ec>
2026-07-19 17:20:19 -0500

    tailwind and other stuff

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title" className="text-sky-500 font-bold">Bienvenido!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
